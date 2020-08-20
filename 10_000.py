b406777b64b5b3eed6316e5985db9c67cb4985b2
Timothee <@>
2020-08-20 19:26:21 +0200

    Bonne_base

---
+++ 10_000.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#10_000.py créé par GoT le 18.08.2020--13:15:00
+
+import.os
+from ramdom import randrange
+
+#################################
+### Déclaration des variables ###
+#################################
+
+score = 0        # score avant lancer de D
+score_2 = 0      # score après lancer de D
+resultat = 0
+relance = True
+nb_D = 6
+D_u              # nombre de D qui ont marqués des points
+
+
+
+while relance:
